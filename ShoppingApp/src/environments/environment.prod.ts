1e4b2e57f6b48d810765da5de0da23ce57cfd38f
Pratik Gare <garep@ad.skynet>
2018-07-27 14:09:40 -0700

    Adds firebase property to both production and local environments

--- ShoppingApp/src/environments/environment.prod.ts
+++ ShoppingApp/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDmyT6BY_faUGhAzy4UKRgWCf5RPYySxpM",
+    authDomain: "shoppingapp-3c0ef.firebaseapp.com",
+    databaseURL: "https://shoppingapp-3c0ef.firebaseio.com",
+    projectId: "shoppingapp-3c0ef",
+    storageBucket: "shoppingapp-3c0ef.appspot.com",
+    messagingSenderId: "340290619912"
+  }
 };
--- ShoppingApp/src/environments/environment.ts
+++ ShoppingApp/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDmyT6BY_faUGhAzy4UKRgWCf5RPYySxpM",
+    authDomain: "shoppingapp-3c0ef.firebaseapp.com",
+    databaseURL: "https://shoppingapp-3c0ef.firebaseio.com",
+    projectId: "shoppingapp-3c0ef",
+    storageBucket: "shoppingapp-3c0ef.appspot.com",
+    messagingSenderId: "340290619912"
+  }
 };
 
 /*
